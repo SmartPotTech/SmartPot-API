c63f122ce0cc2a95b6d75414e12ff9aaebccb5b2
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2024-10-24 17:26:54 -0500

    init

---
+++ SmartPot-API/src/main/java/smarpot/com/api/SmartPotApiApplication.java
@@ -0,0 +1,13 @@
+package smarpot.com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartPotApiApplication.class, args);
+    }
+
+}
