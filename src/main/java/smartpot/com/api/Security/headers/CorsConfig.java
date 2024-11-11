38daa24f1333581563d67deeb56492a659b93853
SebastianLopezO <sebastianlopezosorno2005@gmail.com>
2024-11-10 22:42:17 -0500

    Update CorsConfig.java

--- src/main/java/smartpot/com/api/Security/headers/CorsConfig.java
+++ src/main/java/smartpot/com/api/Security/headers/CorsConfig.java
@@ -2,11 +2,9 @@ package smartpot.com.api.Security.headers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,12 +15,18 @@ import java.util.List;
 public class CorsConfig implements CorsConfigurationSource {
 
     @Value("${http.header.cors.allowedOrigins}")
-    private List<String> allowedOrigins;
+    private String allowedOrigins;
 
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(allowedOrigins.toArray(new String[0])));
+        List<String> originsList;
+        if (allowedOrigins.contains(",")) {
+            originsList = Arrays.asList(allowedOrigins.split(","));
+        } else {
+            originsList = List.of(allowedOrigins);
+        }
+        config.setAllowedOrigins(originsList);
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         return config;
