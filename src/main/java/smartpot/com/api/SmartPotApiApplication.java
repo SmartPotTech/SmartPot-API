a3dd31a0ca97bb6b075c0dbd476592156728a173
SebastianLopezO <sebastianlopezosorno2005@gmail.com>
2024-12-21 13:55:07 -0500

    Dynamic way to load environment variables

--- src/main/java/smartpot/com/api/SmartPotApiApplication.java
+++ src/main/java/smartpot/com/api/SmartPotApiApplication.java
@@ -15,37 +15,15 @@ public class SmartPotApiApplication {
     }
 
     private static void loadEnv(){
-        // Carga de .env
         Dotenv dotenv = Dotenv.load();
 
-        // Configuración APP
-        System.setProperty("APP_NAME", dotenv.get("APP_NAME"));
-        System.setProperty("PORT", dotenv.get("PORT"));
-        System.setProperty("TITLE", dotenv.get("TITLE"));
-        System.setProperty("DESCRIPTION", dotenv.get("DESCRIPTION"));
-        System.setProperty("VERSION", dotenv.get("VERSION"));
-        System.setProperty("AUTHOR", dotenv.get("AUTHOR"));
+        dotenv.entries().forEach(entry -> {
+            String key = entry.getKey();
+            String value = entry.getValue();
 
-        // Conexión de MongoDB
-        System.setProperty("DATA_CONNECTION_METHOD", dotenv.get("DATA_CONNECTION_METHOD"));
-        System.setProperty("DATA_SOURCE_USERNAME", dotenv.get("DATA_SOURCE_USERNAME"));
-        System.setProperty("DATA_SOURCE_PASSWORD", dotenv.get("DATA_SOURCE_PASSWORD"));
-        System.setProperty("DATA_SOURCE_DOMAIN", dotenv.get("DATA_SOURCE_DOMAIN"));
-        System.setProperty("DATA_SOURCE_DB", dotenv.get("DATA_SOURCE_DB"));
-        System.setProperty("DATA_PARAMS", dotenv.get("DATA_PARAMS"));
-
-        // Security Config
-        System.setProperty("SECURITY_JWT_SECRET_KEY", dotenv.get("SECURITY_JWT_SECRET_KEY"));
-        System.setProperty("SECURITY_JWT_EXPIRATION", dotenv.get("SECURITY_JWT_EXPIRATION"));
-        System.setProperty("SECURITY_PUBLIC_ROUTES", dotenv.get("SECURITY_PUBLIC_ROUTES"));
-
-        // Http Headers
-        System.setProperty("HEADER_CORS_ALLOWED_ORIGINS", dotenv.get("HEADER_CORS_ALLOWED_ORIGINS"));
-
-        //Config TOMCAT
-        System.setProperty("SERVER_TOMCAT_TIMEOUT", dotenv.get("SERVER_TOMCAT_TIMEOUT"));
-
-        // Debug
-        System.setProperty("DEBUGGER_MODE", dotenv.get("DEBUGGER_MODE"));
+            if (value != null) {
+                System.setProperty(key, value);
+            }
+        });
     }
 }
