fb9c29cd8d577502392ac7cd7547907c950ef764
SebastianLopezO <sebastianlopezosorno2005@gmail.com>
2025-08-14 10:51:04 -0500

    Update SwaggerConfig.java

--- src/main/java/smartpot/com/api/Documentation/SwaggerConfig.java
+++ src/main/java/smartpot/com/api/Documentation/SwaggerConfig.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,6 +29,8 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "BearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title(title)
@@ -34,6 +39,17 @@ public class SwaggerConfig {
                         .termsOfService("https://github.com/SmarPotTech/SmartPot-API/blob/main/LICENSE")
                         .license(new License().name("MIT License").url("https://opensource.org/license/mit"))
                         .contact(new Contact().name(author).url("https://github.com/SmarPotTech").email("smartpottech@gmail.com"))
+                )
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT"))
                 );
+
+
     }
 }
