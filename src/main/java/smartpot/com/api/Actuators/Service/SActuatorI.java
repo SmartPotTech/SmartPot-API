e1eb81cd9931d9f0b7ed495fe037e4112e253743
andres2204 <medinaandres2204@gmail.com>
2025-11-01 10:33:47 -0500

    SActuatorI Crud Op

--- src/main/java/smartpot/com/api/Actuators/Service/SActuatorI.java
+++ src/main/java/smartpot/com/api/Actuators/Service/SActuatorI.java
@@ -1,6 +1,8 @@
 package smartpot.com.api.Actuators.Service;
 
+import org.springframework.http.ResponseEntity;
 import smartpot.com.api.Actuators.Model.Entity.Actuator;
+import smartpot.com.api.Exception.ApiResponse;
 
 import java.util.List;
 
@@ -10,4 +12,12 @@ public interface SActuatorI {
     Actuator getActuatorById(String id);
 
     List<Actuator> getActuatorsByCrop(String crop);
+
+    Actuator CreateActuator(Actuator actuator);
+
+    Actuator UpdateActuator(Actuator actuator);
+
+    Actuator DeleteActuatorById(String id);
+
+    ResponseEntity<ApiResponse> DeleteActuators(List<String> ids);
 }
