fdfe65f99095ba853f7f1108a7cb2fcb4823e6da
andres2204 <medinaandres2204@gmail.com>
2025-11-15 16:15:58 -0500

    Controller now returns a ResponseEntity

--- src/main/java/app/smartpot/api/actuators/controller/ActuatorController.java
+++ src/main/java/app/smartpot/api/actuators/controller/ActuatorController.java
@@ -1,6 +1,8 @@
 package app.smartpot.api.actuators.controller;
 
+import app.smartpot.api.responses.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import app.smartpot.api.actuators.model.dto.ActuatorDTO;
@@ -13,6 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/Actuators")
 public class ActuatorController {
+
+    // TODO: Documentar mejor, por ejemplo como en UserController
 
     private final ServiceActuator serviceActuator;
 
@@ -28,45 +32,70 @@ public class ActuatorController {
      * @return Lista de todos los actuadores existentes
      */
     @GetMapping("/All")
-    public List<Actuator> getAllActuators() {
-        return serviceActuator.getAllActuators();
+    public ResponseEntity<?> getAllActuators() {
+        try {
+            return new ResponseEntity<>(serviceActuator.getAllActuators(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ErrorResponse("Error al buscar los actuadores [" + e.getMessage() + "]", HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+        }
     }
 
     /**
-     * Obtiene el actuadores con el identificador proporcionado.
+     * Obtiene el actuador con el identificador proporcionado.
      *
-     * @param id Es el identificador del la instruccion del actuador
-     * @return Lista de todos los actuadores existentes
+     * @param id Es el identificador del actuador
+     * @return El actuador que tenga la id suministrada
      */
     @GetMapping("/{id}")
-    public Actuator getActuatorById(@PathVariable String id) {
-        return serviceActuator.getActuatorById(id);
+    public ResponseEntity<?> getActuatorById(@PathVariable String id) {
+        try {
+            return new ResponseEntity<>(serviceActuator.getActuatorById(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ErrorResponse("Error al buscar los actuadores [" + e.getMessage() + "]", HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+        }
     }
 
     /**
-     * Obtiene todos los actuadores almacenados en el sistema para un cultivo espesificado.
+     * Obtiene todos los actuadores almacenados en el sistema para un cultivo especificado.
      *
      * @param id Es el identificador del cultivo
      * @return Lista de todos los actuadores existentes
      */
     @GetMapping("/crop/{id}")
-    public List<Actuator> getActuatorByCrop(@PathVariable String id) {
-        return serviceActuator.getActuatorsByCrop(id);
+    public ResponseEntity<?> getActuatorByCrop(@PathVariable String id) {
+        try {
+            return new ResponseEntity<>(serviceActuator.getActuatorsByCrop(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ErrorResponse("Error al buscar el actuadores [" + e.getMessage() + "]", HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+        }
+
     }
 
     @PostMapping("/Create")
-    public Actuator createActuator(@RequestBody ActuatorDTO actuator) throws Exception {
-        return serviceActuator.createActuator(actuator);
+    public ResponseEntity<?> createActuator(@RequestBody ActuatorDTO actuator) {
+        try {
+            return new ResponseEntity<>(serviceActuator.createActuator(actuator), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ErrorResponse("Error al crear el actuador [" + e.getMessage() + "]", HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+        }
     }
 
     @PutMapping("/Update/{id}")
-    public Actuator updateActuator(@PathVariable String id, @RequestBody ActuatorDTO actuator) throws Exception {
-        return serviceActuator.updateActuator(serviceActuator.getActuatorById(id), actuator);
+    public ResponseEntity<?> updateActuator(@PathVariable String id, @RequestBody ActuatorDTO actuator) {
+        try {
+            return new ResponseEntity<>(serviceActuator.updateActuator(serviceActuator.getActuatorById(id), actuator), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ErrorResponse("Error al actualizar el actuador [" + e.getMessage() + "]", HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/Delete/{id}")
-    public ResponseEntity<ApiResponse> deleteActuator(@PathVariable String id) throws Exception {
-        return serviceActuator.deleteActuatorById(serviceActuator.getActuatorById(id));
+    public ResponseEntity<?> deleteActuator(@PathVariable String id) {
+        try {
+            return new ResponseEntity<>(serviceActuator.deleteActuatorById(serviceActuator.getActuatorById(id).getId()), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ErrorResponse("Error al eliminar el actuador [" + e.getMessage() + "]", HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+        }
     }
 
 }
